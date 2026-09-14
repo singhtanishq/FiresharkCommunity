8e0c17c3d7d2514fd6ffb37c535750eec5966dc6
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-14 19:06:20 +0530

    Update VerificationType.php

--- backend/app/Enums/VerificationType.php
+++ backend/app/Enums/VerificationType.php
@@ -5,7 +5,6 @@ namespace App\Enums;
 enum VerificationType: string
 {
     case Team = 'team';
-    case Instructor = 'instructor';
     case Expert = 'expert';
     case Alumni = 'alumni';
     case Professional = 'professional';
